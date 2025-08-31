988efe6275f39584af282375af73c19989f0f57f
MKeshav04 <keshav04mishra@gmail.com>
2025-08-31 21:12:17 +0530

    DEPLOY: Finalize live URLs and complete full-stack application

--- backend/api.py
+++ backend/api.py
@@ -18,7 +18,7 @@ app.add_middleware(
     allow_origins=["http://localhost:3000", 
                    "http://localhost:3001", 
                    "http://localhost:3002", 
-                   "https://infinite-tic-tac-toe-six.vercel.app/"], # Allows your React app to talk to the API
+                   "https://infinite-tic-tac-toe-six.vercel.app" ], # Allows your React app to talk to the API
     allow_credentials=True,
     allow_methods=["*"], # Allows all methods (GET, POST, etc)
     allow_headers=["*"],
